e92a54985a3ac1bd7c812bbe3eca531d75dd30df
Ashton Wiersdorf <ashton.wiersdorf@tazworks.com>
2020-12-17 11:25:08 -0700

    update transformer.rkt

--- transformer.rkt
+++ transformer.rkt
@@ -1,23 +1,5 @@
 #lang racket
 
-(struct tm () #:transparent)
-(struct var tm (name) #:transparent)
-(struct abst tm (arg body) #:transparent)
-(struct app tm (fexpr aexpr) #:transparent)
-(struct kapp tm (cexpr aexpr) #:transparent)
-(struct ttrue tm () #:transparent)
-(struct tfalse tm () #:transparent)
-(struct tif tm (test tc fc) #:transparent)
-
-(define (cps-struct expr k)
-  (match expr
-    [(var name) (kapp k (var name))]
-    [(ttrue) (kapp k (ttrue))]
-    [(tfalse) (kapp k (tfalse))]
-    [(abst arg body)
-     (let ([k₀ (gensym)])
-       (kapp k (abst arg (abst k₀ (cps-struct body k₀)))))]))
-
 (define/match (all? pred? lst)
   [(_ '()) #t]
   [(pred? (cons hd tl)) (and (pred? hd) (all? pred? tl))])
